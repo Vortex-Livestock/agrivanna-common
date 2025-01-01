0c2e7c3048b37520c046e9b5ecf97bf105e642c4
Axeloooo <axelshz@gmail.com>
2025-01-01 16:40:39 -0700

    feat: AG-76 Implemented write and read json functions

---
+++ json.go
@@ -0,0 +1,16 @@
+package common
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func WriteJSON(w http.ResponseWriter, statusCode int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func ReadJSON(r *http.Request, data any) error {
+	return json.NewDecoder(r.Body).Decode(data)
+}
